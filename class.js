3fafd47c18f01e34f74df3e3e529c34259c6ef0c
Arafcoder885 <jarifaraf15@gmail.com>
2025-12-06 16:44:47 +0600

    Add files via upload

---
+++ class.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Student extends React.Component {
+    render() {
+        return <h2>Hi, I am Codingal Student</h2>;
+    }
+}
+ReactDOM.render(<Student />, document.getElementById('root'))
