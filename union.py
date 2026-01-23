76b7fdf3fe9f47756d80086c74acf421d9763f8d
Arafcoder885 <jarifaraf15@gmail.com>
2026-01-23 16:57:10 +0600

    Add files via upload

---
+++ union.py
@@ -0,0 +1,8 @@
+setc1 = {"green", "blue"}
+setc2 = {"blue", "yellow"}
+print("original setc: ")
+print(setc1)
+print(setc2)
+setc = setc1.union(setc2)
+print("union of setc1 and setc2: ")
+print(setc)
